022cb7345ef6c291761aa48d57bfdb9cb2f3637b
Jason Anca <jason_anca@hotmail.com>
2022-05-18 18:26:11 +0100

    Changes for graphs nodes
    Updated the adding of graphs nodes, created connections between nodes, updated UI, fixed a bug with name of Room 17A

--- src/main/java/com/example/national_gallery_ca/GraphLinkAL.java
+++ src/main/java/com/example/national_gallery_ca/GraphLinkAL.java
@@ -1,13 +1,20 @@
 package com.example.national_gallery_ca;
 
-import java.util.ArrayList;
-import java.util.List;
+
+//taken from notes
+
 
 public class GraphLinkAL {
-    public GraphNodeAL2<?> destNode; //Could also store source node if required
+
+    public GraphNodeAL<?> destNode;//Could also store source node if required
+
     public int cost; //Other link attributes could be similarly stored
-    public GraphLinkAL(GraphNodeAL2<?> destNode,int cost) {
+
+
+    public GraphLinkAL(GraphNodeAL<?> destNode,int cost) {
         this.destNode=destNode;
         this.cost=cost;
     }
 }
+
+
---
+++ src/main/java/com/example/national_gallery_ca/GraphNodeAL.java
@@ -0,0 +1,24 @@
+package com.example.national_gallery_ca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphNodeAL<T> {
+
+    public T data;
+    public int nodeValue= Integer.MAX_VALUE;
+
+    public List<GraphLinkAL> adjList = new ArrayList<>(); //Adjacency list now contains link objects = key!
+    //Could use any concrete List implementation
+    public GraphNodeAL(T data) {
+        this.data=data;
+    }
+
+    public void connectToNodeDirected(GraphNodeAL<T> destNode,int cost) {
+        adjList.add( new GraphLinkAL(destNode,cost) ); //Add new link object to source adjacency list
+    }
+    public void connectToNodeUndirected(GraphNodeAL<T> destNode,int cost) {
+        adjList.add( new GraphLinkAL(destNode,cost) ); //Add new link object to source adjacency list
+        destNode.adjList.add( new GraphLinkAL(this,cost) ); //Add new link object to destination adjacency list
+    }
+}
