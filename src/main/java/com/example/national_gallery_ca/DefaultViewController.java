576623318bfa88ec5e006f4781d7218a44aecf84
Jason Anca <jason_anca@hotmail.com>
2022-05-19 12:18:59 +0100

    bruh
    bruh

--- src/main/java/com/example/national_gallery_ca/DefaultViewController.java
+++ src/main/java/com/example/national_gallery_ca/DefaultViewController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -231,13 +230,47 @@ public class DefaultViewController {
         return findPathBreadthFirst(agenda,encountered,lookingfor); //Tail call
     }
 
-    public void BFS(ActionEvent actionEvent){
+//    public void BFS(ActionEvent actionEvent){
+//        List<GraphNodeAL<?>> pathList = new LinkedList<>();
+//        CostedPath cp = findPathBreadthFirst(getNode(start.getValue(), getNode(finish.getValue()).data));
+//        pathList = cp.pathList;
+//        drawLine(pathList, color);
+//    }
+
+    public void DFS(ActionEvent actionEvent){
         List<GraphNodeAL<?>> pathList = new LinkedList<>();
-        CostedPath cp = findPathBreadthFirst(getNode(start.getValue(),));
-        System.out.println(cp.pathList.size());
+        CostedPath cp = searchGraphDepthFirstCheapestPath(getNode(start.getValue(),null,0, getNode(finish.getValue()).data));
         pathList = cp.pathList;
         drawLine(pathList, color);
     }
+
+    //Retrieve cheapest path by expanding all paths recursively depth-first
+    public static <T> CostedPath searchGraphDepthFirstCheapestPath(GraphNodeAL<?> from, List<GraphNodeAL<?>> encountered,int totalCost, T lookingfor){
+
+        if(from.data.equals(lookingfor)){ //Found it - end of path
+            CostedPath cp=new CostedPath(); //Create a new CostedPath object
+            cp.pathList.add(from); //Add the current node to it - only (end of path) element
+            cp.pathCost=totalCost; //Use the current total cost
+            return cp; //Return the CostedPath object
+        }
+        if(encountered==null) encountered=new ArrayList<>(); //First node so create new (empty) encountered list
+        encountered.add(from);
+        List<CostedPath> allPaths=new ArrayList<>(); //Collection for all candidate costed paths from this node
+        for(GraphLinkAL adjLink : from.adjList) //For every adjacent node
+            if(!encountered.contains(adjLink.destNode)) //That has not yet been encountered
+            {
+//Create a new CostedPath from this node to the searched for item (if a valid path exists)
+                CostedPath temp=searchGraphDepthFirstCheapestPath(adjLink.destNode,new ArrayList<>(encountered),
+                        totalCost+adjLink.cost,lookingfor);
+                if(temp==null) continue; //No path was found, so continue to the next iteration
+                temp.pathList.add(0,from); //Add the current node to the front of the path list
+                allPaths.add(temp); //Add the new candidate path to the list of all costed paths
+            }
+//If no paths were found then return null. Otherwise, return the cheapest path (i.e. the one with min pathCost)
+        return allPaths.isEmpty() ? null : Collections.min(allPaths, (p1,p2)->p1.pathCost-p2.pathCost);
+    }
+
+
 
     public static <T> List<GraphNodeAL<?>> findPathDepthFirst(GraphNodeAL<?> from, List<GraphNodeAL<?>> encountered, T lookingfor){
         List<GraphNodeAL<?>> result;
